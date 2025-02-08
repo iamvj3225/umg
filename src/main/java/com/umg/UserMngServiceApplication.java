4f0e16fdddcb68d8a8804d0360a28b0b4bfffc14
vijay <iamvijaygirange@gmail.com>
2025-02-09 00:26:01 +0530

    test

---
+++ src/main/java/com/umg/UserMngServiceApplication.java
@@ -0,0 +1,13 @@
+package com.umg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMngServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserMngServiceApplication.class, args);
+	}
+
+}
